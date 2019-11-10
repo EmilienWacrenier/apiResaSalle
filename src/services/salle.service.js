122a4733f5021634cdd118443820dfe5974ef343
Brocel <geenens.prof@gmail.com>
2019-11-10 13:45:59 +0100

    try catch findsalles et findsallesbyid

--- src/services/salle.service.js
+++ src/services/salle.service.js
@@ -2,8 +2,13 @@ const salleBuilder = require('../builders/salle.builder');
 //get all salles
 module.exports.get_salles = () => {
   return new Promise(async (resolve, reject) => {
-    const salles = await salleBuilder.findSalles();
-    resolve(salles);
+    try {
+      const salles = await salleBuilder.findSalles();
+      resolve(salles);
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
   });
 };
 //get les salles occupées (par journée)
@@ -21,10 +26,15 @@ module.exports.get_salles_booked_today = function () {
 //get salle by id
 module.exports.get_salle = (params) => {
   return new Promise(async (resolve, reject) => {
-    const {
-      id
-    } = params;
-    const salle = await salleBuilder.findSalle(id);
-    resolve(salle);
+    try {
+      const {
+        id
+      } = params;
+      const salle = await salleBuilder.findSalle(id);
+      resolve(salle);
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
   });
 };
