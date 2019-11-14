50d6ebacb4195fbc77727e820f816661f4ab32e0
EmilienWacrenier <emilien.wacrenier@atos.net>
2019-11-14 14:50:55 +0100

    modification des nom  de route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,18 +18,11 @@ getCode = function(code){
 
 
 exports.getUsers = async (req, res) => {
-    //if(jwt.testToken(req)){
         let data = await userService.get_users();
         return res.status(200).json(data);
-    //}
-    //else{
-        return res.status(400).json({'Erreur':'Wrong Token'});
-    //}
 }
 
 exports.getUser  = async (req, res) => {
-    //let data = await userService.get_user(req);
-    //return res.status(200).json(data);
     let data = await recurrenceService.createRecurrence(req);
     return res.status(200).json(data);
 }
