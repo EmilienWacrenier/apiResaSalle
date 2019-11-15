4631216f4142cda44660b6a53a07e79acfc1d552
Brocel <geenens.prof@gmail.com>
2019-11-15 10:23:36 +0100

    transfert des méthodes getSallesBooked... dans reservation

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -1,4 +1,5 @@
 const reservationService = require('../services/reservation.service');
+
 //créer 1 réservation
 exports.creerReservation = async (req,res) => {
     let data = await reservationService.create_reservation(req);
@@ -14,4 +15,19 @@ exports.getReservations = async (req, res) => {
 exports.getReservationById = async (req, res) => {
         let data = await reservationService.get_reservation_by_id(req.params);
         return res.status(200).json(data);
+}
+//get salles booked
+exports.getSallesBooked = async (req, res) => {
+    if (!req.body.endDate) {
+        let data = await reservationService.get_salles_booked_by_day(req);
+        return res.status(200).json(data);
+    } else {
+        let data = await reservationService.get_salles_booked_between(req);
+        return res.status(200).json(data);
+    }
+}
+//get réservations  by ID_salle
+exports.getSallesBookedById = async (req,res) => {
+    let data = await reservationService.get_salles_booked_by_id(req);
+    return res.status(200).json(data);
 }
