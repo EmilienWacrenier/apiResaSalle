c3bdeab81b5c65073f820a6a93c0399dcab3b20c
Brocel <geenens.prof@gmail.com>
2019-11-15 15:52:04 +0100

    correctif result dans controller

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -8,30 +8,30 @@ exports.creerReservation = async (req, res) => {
 
 exports.getReservations = async (req, res) => {
     let data = await reservationService.get_reservations();
-    return res.status(data.code).json(data.result);
+    return res.status(data.code).json({ result: data.result });
 }
 
 exports.getReservationById = async (req, res) => {
     let data = await reservationService.get_reservation_by_id(req.params);
-    return res.status(data.code).json(data.result);
+    return res.status(data.code).json({ result: data.result });
 }
 
 exports.getSallesBooked = async (req, res) => {
     if (!req.body.endDate) {
         let data = await reservationService.get_salles_booked_by_day(req);
-        return res.status(data.code).json(data.result);
+        return res.status(data.code).json({ result: data.result });
     } else {
         let data = await reservationService.get_salles_booked_between(req);
-        return res.status(data.code).json(data.result);
+        return res.status(data.code).json({ result: data.result });
     }
 }
 
 exports.getSallesBookedById = async (req, res) => {
     let data = await reservationService.get_salles_booked_by_id(req);
-    return res.status(data.code).json(data.result);
+    return res.status(data.code).json({ result: data.result });
 }
 
 exports.getReservationsByUserId = async (req, res) => {
     let data = await reservationService.get_reservations_by_user_id(req);
-    return res.status(data.code).json(data.result);
+    return res.status(data.code).json({ result: data.result });
 }
