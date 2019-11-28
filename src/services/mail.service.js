89b4926c789835937ce97a7891281e68140d0a23
Brocel <geenens.prof@gmail.com>
2019-11-28 13:51:12 +0100

    test mailService03

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -20,68 +20,79 @@ let transporter = nodemailer.createTransport({
 });
 // Envoi d'un mail aux participants
 module.exports.send_mail = (req) => {
-    //Récupération des parametres de requete
-    const senderId = req.body.userId;
-    const recieversIds = req.body.users;
-    const object = req.body.object;
-    const startDate = req.body.startDate;
-    const startTime = moment(startDate).format('HH:mm');
-    const roomId = req.body.roomId;
-    //Vérification des parametres
-    console.log(senderId);
-    console.log(recieversIds);
-    console.log(object);
-    console.log(roomId);
-    //Récupération des données
-    //sender
-    const req2 = {
-        query: {
-            userId: senderId
-        }
-    };
-    const sender = userBuilder.findUserById(req2);
-    console.log('sender : ' + sender);
-    const senderMail = sender.email;
-    //recievers
-    const recieversMail = [];
-    for (const idUser of recieversIds) {
-        req3 = {
-            query: {
-                userId: idUser
-            }
-        }
-        let participant = userBuilder.findUserById(req3);
-        recieversMail.push(participant.email);
-    };
-    //room
-    const room = salleBuilder.findSalle(roomId);
-    const roomName = room.nom;
-    //verification du smtp
-    if (!this.verifiy_smtp()) {
-        console.log('SMTP error !!');
-    };
-    //Verification des parametre
-    console.log('mail du sender : ' + senderMail);
-    console.log('mails des recievers : ' + recieversMail);
-    console.log('nom de la salle : ' + roomName);
-    console.log('objet de la réunion : ' + object);
-    console.log('date de la réunion : ' + startDate);
-    console.log('heure de la réunion : ' + startTime);
-    if (!senderMail||!recieversMail||!object||!startDate||!startTime||!roomName) {
-        return console.log('Il manque un paramètre');
-    };
-    let sendMail = transporter.sendMail(this.mail_config(senderMail, recieversMail, object, startDate, startTime, roomName), function(error,info){
-        if(error) {
-            console.log(error);
-            return error;
-        } else {
-            console.log('Message sent: ' + info.response);
+    return new Promise(async (resolve, reject) => {
+        try {
+            //Récupération des parametres de requete
+            const senderId = req.body.userId;
+            const recieversIds = req.body.users;
+            const object = req.body.object;
+            const startDate = req.body.startDate;
+            const startTime = moment(startDate).format('HH:mm');
+            const roomId = req.body.roomId;
+            //Vérification des parametres
+            console.log(senderId);
+            console.log(recieversIds);
+            console.log(object);
+            console.log(roomId);
+            //Récupération des données
+            //sender
+            const req2 = {
+                query: {
+                    userId: senderId
+                }
+            };
+            const sender = await userBuilder.findUserById(req2);
+            console.log('sender : ' + sender);
+            const senderMail = sender.email;
+            //recievers
+            const recieversMail = [];
+            for (const idUser of recieversIds) {
+                req3 = {
+                    query: {
+                        userId: idUser
+                    }
+                }
+                let participant = await userBuilder.findUserById(req3);
+                recieversMail.push(participant.email);
+            };
+            //room
+            const room = await salleBuilder.findSalle(roomId);
+            console.log('room : '  + room);
+            const roomName = room.name;
 
-        }
-        return sendMail;
-        console.log(sendMail);
-    });
+            //Verification des parametre
+            console.log('mail du sender : ' + senderMail);
+            console.log('mails des recievers : ' + recieversMail);
+            console.log('nom de la salle : ' + roomName);
+            console.log('objet de la réunion : ' + object);
+            console.log('date de la réunion : ' + startDate);
+            console.log('heure de la réunion : ' + startTime);
+            if (!senderMail||!recieversMail||!object||!startDate||!startTime||!roomName) {
+                return console.log('Il manque un paramètre');
+            };
+            let sendMail = transporter.sendMail(this.mail_config(senderMail, recieversMail, object, startDate, startTime, roomName), function(error,info){
+                //verification du smtp
+                var verifySMTP = this.verifiy_smtp();
+                if (!verifySMTP) {
+                    console.log('SMTP error !!');
+                };
+                if(error) {
+                    console.log(error);
+                    return error;
+                } else {
+                    console.log('Message sent: ' + info.response);
 
+                }
+                return sendMail;
+                console.log(sendMail);
+            });
+        }  catch (err) {
+            return resolve({
+                code: 500,
+                result: err
+            });
+        }
+});
 };
 //Configuration du message
 module.exports.mail_config = (sender, recievers, object, startDate, startTime, room) => {
